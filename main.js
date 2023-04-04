4536af97d15589397ee872a9f23e0ddf046b355e
JingWang12345 <127298095+JingWang12345@users.noreply.github.com>
2023-04-04 13:26:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-"use strict"
+// "use strict"
 
 let myName
 
@@ -9,3 +9,38 @@ function setName(newName) {
 setName("Walter") // Error!!!
 console.log("myName: " + myName);
 console.log("myNam: " + myNam);
+
+// shadowing
+
+
+
+
+
+// Linguaggi dinamici 
+
+
+
+
+
+// Annotazioni di tipo
+
+// bool -> bool
+
+
+
+
+// Tipi generici
+
+function test(testDescription, body) {
+    if (body()) {
+        console.log(`TEST SUCCEEDED: ${testDescription}`);
+        
+    }else {
+        console.log(`TEST FAILED: ${testDescription}`);   
+    }
+    
+}
+
+test("picking first element of array of strings", function(){
+    return 
+})
