7d7709dfebea7184abedcecce231e8d3f7556f01
JingWang12345 <127298095+JingWang12345@users.noreply.github.com>
2023-04-04 13:26:23 +0200

    bug

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let myName
+
+function setName(newName) {
+    myNam = newName
+}
+
+setName("Walter") // Error!!!
+console.log("myName: " + myName);
+console.log("myNam: " + myNam);
